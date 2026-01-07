1d5bbe7ed9ec75a7504ec26c81eb57d8d57b4846
JaimeCKY <160580330+JaimeCKY@users.noreply.github.com>
2026-01-07 00:34:49 -0600

    Implement stack push logic with position management

--- Stack.java
+++ Stack.java
@@ -10,12 +10,66 @@ public Stack(int s){
 
 /** Push Inserta un elemento en la parte superior de la pila.  **/
 public void push(V v) {
-  
-    
+
+ p0(); 
     
 
     
 }
+
+private void p0(){
+  if(pos == e){
+    p1();
+    
+  } else if(pos < e){
+    p2();
+    
+  } else if(pos == b){
+    
+    
+  }
+
+  
+}
+
+private void p1(){
+
+  if(cur == e){
+    
+    cur -= 1;
+    
+  } else if(cur > b){
+
+    cur -= 1;
+    
+  } else if(cur == b){
+
+    cur -= 1;
+    
+  } else if(cur == -1){
+
+  /** error message **/
+    
+  }
+
+  
+}
+
+  private void p2(){
+    
+  
+,  ,  2,  4,  5,  8,  3,  ,  ,  ,
+	      cur	       pos	  e
+	      				mov = e - pos	
+	      				while(pos != cur)
+		      				stack[pos + mov]
+		      				pos--
+    
+
+
+    
+  }
+  
   
     
 /** Pop  Elimina el elemento superior y lo devuelve.  **/
